1bd7a5ccce098ee6784785bb58e4f9d3ad47baaf
John Holdsworth <github@johnholdsworth.com>
2016-12-15 16:22:39 +0000

    Updated for slid load addresses

--- Symbolicate/symbolicate.pl
+++ Symbolicate/symbolicate.pl
@@ -12,20 +12,29 @@ use strict;
 my ($crash, $archive) = @ARGV;
 my ($app, $ident, $pid, $llin, $llout);
 
+open CRASH, "< $crash";
+my ($loadAddress) = join( '', <CRASH>) =~ /Binary Images:\s+(\S+)/;
+
+warn "$crash, $archive, $loadAddress\n";
+
 open CRASH, "< $crash" or die "Could not open '$crash' as: $!";
 
 while ( my $line = <CRASH> ) {
     chomp $line;
 
-    if ( $line =~ m@^Path:.*/MacOS/([^/]+)$@ ) {
-        $app = $1;
-
+    if ( ($app) = $line =~ m@^Process:\s+(\S+)@ ) {
         chomp( my $binary = `find '$archive' -regex '.*/MacOS/$app'` );
         chomp( my $symbol = `find '$archive' -regex '.*/DWARF/$app'` );
 
+        warn "$binary, $symbol\n";
+
         system "rm -rf tmp && mkdir tmp && cd tmp && ln -s '$binary' . && ln -s '$symbol' $app.dSYM";
         $pid = open2 $llout, $llin, "cd tmp; lldb $app" or die "Could not open lldb";
-        #warn ">>>".
+        <$llout>;
+        <$llout>;
+        print $llin "target modules load --file $app __TEXT $loadAddress\n";
+        flush $llin;
+        <$llout>;
         <$llout>;
         sleep 1;
     }
@@ -33,20 +42,20 @@ while ( my $line = <CRASH> ) {
         $ident = $1;
     }
     elsif ( $ident and my ($prefix, $addr) = $line =~ /^(\d+\s+$ident\s+)(\S+)/ ) {
-        print $llin "image lookup -v --address $addr\n";
+        print $llin "image lookup --address $addr\np 1\n";
         flush $llin;
-        #warn ">>".
-        <$llout>;
+        warn "??".<$llout>;
 
         my ($ch, $summary);
-        while( read $llout, $ch, 1 and ($ch ne "(" and $ch ne "\n" or
-            $ch eq "\n" and read $llout, $ch, 1 and $ch ne "(") and my $out = <$llout> ) {
+        while( read $llout, $ch, 1 and ($ch ne "(") and my $out = <$llout> ) {
+                warn ">> $out";
                 ($summary) = $out =~ /Summary: (.*)/ if !$summary;
                 #print ">".$out;
         }
 
-        read $llout, $ch, length "lldb) ";
         $line = "$prefix$summary";
+        <$llout>;
+        <$llout>;
     }
 
     print "$line\n";
